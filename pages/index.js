98b0e5691365598823043503e8f5801710895cf6
Tolaj <swapnilhgf@gmail.com>
2024-11-02 06:11:56 -0400

    fix

--- pages/index.js
+++ pages/index.js
@@ -73,7 +73,6 @@ export default function Index() {
 	  	<button onClick={()=>{setDialog(false)}} className="absolute top-2 right-3 text-gray-500 hover:text-gray-700 focus:outline-none" >
 			✕
 		</button>
-		{browserName}
 		<h2 className="text-xl font-bold mb-4 text-center">Install App</h2>
 	
 		<div className="text-sm text-gray-600">
