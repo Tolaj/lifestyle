217af15c3f707c5c1956115a20753bc4773fcd47
Tolaj <swapnilhgf@gmail.com>
2024-11-04 03:51:05 -0500

    fix

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const withPWA = require('next-pwa')({
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   //  redirects: async () => {
   //   return [
   //     {
