18d902e6a694bf4ff47e3dc40a4c3779d46d07d4
Tolaj <swapnilhgf@gmail.com>
2026-06-07 08:46:28 -0400

    fix: add cart button

--- components/InputFields/index.js
+++ components/InputFields/index.js
@@ -305,16 +305,37 @@ const GridButton = (props) => {
                 </div>
             </>)
         case "ADD_CART":
-            return (<>
-                <div title={props.title} onClick={props.onClick} className={`${props.className} relative `} >
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-3 absolute top-0 right-0 bg-white rounded-full">
-                        <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14Zm.75-10.25v2.5h2.5a.75.75 0 0 1 0 1.5h-2.5v2.5a.75.75 0 0 1-1.5 0v-2.5h-2.5a.75.75 0 0 1 0-1.5h2.5v-2.5a.75.75 0 0 1 1.5 0Z" clip-rule="evenodd" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-black hover:cursor-pointer flex flex-grow ">
+            return (
+                <div
+                    title={props.title}
+                    onClick={props.onClick}
+                    className={`${props.className} inline-grid place-items-center`}
+                >
+                    {/* Cart Icon */}
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="w-6 h-6 text-black hover:cursor-pointer col-start-1 row-start-1"
+                    >
                         <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
                     </svg>
+
+                    {/* Plus Icon */}
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        fill="currentColor"
+                        className="size-3 bg-white rounded-full col-start-1 row-start-1 self-start justify-self-end "
+                    >
+                        <path
+                            fillRule="evenodd"
+                            d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14Zm.75-10.25v2.5h2.5a.75.75 0 0 1 0 1.5h-2.5v2.5a.75.75 0 0 1-1.5 0v-2.5h-2.5a.75.75 0 0 1 0-1.5h2.5v-2.5a.75.75 0 0 1 1.5 0Z"
+                            clipRule="evenodd"
+                        />
+                    </svg>
                 </div>
-            </>)
+            );
         default:
             return (<><div className='bg-black text-white text-xs p-1 rounded-xl'>No Button Type Provided</div></>)
 
